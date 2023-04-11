c824a7029f916a4d3503cea457e2fc03a68da419
Jacob Diamond <jacobdiamond286@outlook.com>
2023-04-11 12:59:11 -0600

    Created Main app and designed sqlite database

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def login(request):
+    return render(request, "main/login.html")
