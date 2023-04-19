fdd7d41d6b1b8b0ae62f933d274f771bcbf5c678
Jacob Diamond <jacobdiamond286@outlook.com>
2023-04-19 16:23:13 -0600

    Created MySQL Database

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Deeznuts1!'
+
+    )
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE bugtracker")
+
+print("All Done")
