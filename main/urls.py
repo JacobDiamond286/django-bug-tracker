bcd19b5f97fbc0599a9b9352ba992a1a0203c540
Jacob Diamond <jacobdiamond286@outlook.com>
2023-05-05 17:31:34 -0600

    Finished login and logout functions and views

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
 from . import views
 
-app_name = 'main'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("signup/", views.signup , name="signup"),
     path("team/", views.team , name="team")
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
@@ -7,19 +7,29 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
-
-def login(request):
-    return render(request, "main/login.html")
+    #check to see if logging in
+    if request.method == "POST":
+        username = request.POST['uname']
+        password = request.POST['psw']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('index')
+        else:
+            messages.success(request, "There was an error logging in, please try again.")
+            return redirect('index')
+    else:
+        return render(request, 'main/index.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('index')
 
 def signup(request):
     return render(request, "main/signup.html")
 
 def team(request):
     return render(request, "main/team.html")
-
-def login_user(request):
-    pass
-
-def logout_user(request):
-    pass
