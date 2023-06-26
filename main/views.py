f9846d1aec4c73f8b705be75be61303d8e7a1c05
Jacob Diamond <jacobdiamond286@outlook.com>
2023-06-26 16:48:18 -0600

    Register Getting Error Involving User, Should rewatch video

--- main/views.py
+++ main/views.py
@@ -3,14 +3,15 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
 def index(request):
     #check to see if logging in
     if request.method == "POST":
-        username = request.POST['uname']
-        password = request.POST['psw']
+        username = 'uname' in request.POST
+        password = 'psw' in request.POST
         #Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -29,7 +30,22 @@ def logout_user(request):
     return redirect('index')
 
 def register_user(request):
-    return render(request, 'main/register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have succesfully registered, Welcome!")
+            return redirect('index')
+    else:
+        form = SignUpForm()
+        return render(request, 'main/register.html', {'form':form})
+    
+    return render(request, 'main/register.html', {'form':form})
 
 def team(request):
     return render(request, "main/team.html")
